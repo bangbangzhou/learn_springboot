b8d3d11a7aa8ac75905c5fcfeb453f006af26cb0
springboot葵花宝典 <zbbmeta@qq.com>
2023-09-06 00:34:55 +0800

    30-nacos注册中心

---
+++ day29-eureka-loadbalancer/eureka-loadbalancer-order-service/src/main/java/com/zbbmeta/order/config/CustomLoadBalancerConfiguration.java
@@ -0,0 +1,28 @@
+package com.zbbmeta.order.config;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+
+/*
+*
+ * @author springboot葵花宝典
+ * @description: TODO
+ */
+
+/*public class CustomLoadBalancerConfiguration {
+
+    @Bean
+    ReactorLoadBalancer<ServiceInstance> randomLoadBalancer(Environment environment,
+                           LoadBalancerClientFactory loadBalancerClientFactory) {
+        String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+        return new RandomLoadBalancer(loadBalancerClientFactory
+                .getLazyProvider(name, ServiceInstanceListSupplier.class),
+                name);
+    }
+}*/
