ee505ecdfc59e5c32083b80928f7cb72378705e3
springboot葵花宝典 <zbbmeta@qq.com>
2023-09-07 23:32:22 +0800

    nacos配置中心

--- day30-cloud-nacos-config/nacos-config-user-oservice/src/main/java/com/zbbmeta/user/controller/UserController.java
+++ day30-cloud-nacos-config/nacos-config-user-oservice/src/main/java/com/zbbmeta/user/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.zbbmeta.user.controller;
 
 import com.zbbmeta.user.entity.User;
+import com.zbbmeta.user.properties.PatternProperties;
 import com.zbbmeta.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,20 +20,27 @@ import java.time.format.DateTimeFormatter;
  */
 @RequestMapping("/user")
 @RestController
+@RefreshScope
 public class UserController {
 
 
     @Autowired
     private UserService userService;
+//
+//    @Value("${pattern.dateformat}")
+//    private String dateformat;
+    @Autowired
+    private PatternProperties patternProperties;
 
-/*
-    @GetMapping("/now")
+    @GetMapping("now")
     public String now(){
-        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(patternProperties.getDateformat()));
     }
 
-*/
-
+    @GetMapping("prop")
+    public PatternProperties prop(){
+        return patternProperties;
+    }
 
     @Value("${server.port}")
     private String  port;
